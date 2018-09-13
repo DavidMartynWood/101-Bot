d06873399519ffad6f8860bff2f74a28cbce817c
Anthony Hughes <anthony@anthonyhughes.work>
2018-09-13 11:42:08 +0100

    Fix bad serialization

--- NonEmergencyBot/NonEmergencyBot/LUIS/LuisContext.cs
+++ NonEmergencyBot/NonEmergencyBot/LUIS/LuisContext.cs
@@ -11,10 +11,10 @@ namespace NonEmergencyBot.LUIS
     [Serializable]
     public class LuisContext
     {
-        public const string QueryUri = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/" +
-            "c00dcea1-260f-4fe9-b3aa-13abff2f2d22" +
+        public readonly string QueryUri = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/" +
+            PrivateKeys.LUISApiKey +
             "?subscription-key=" +
-            "a78c19171ea841e6a80961c8c6ce6e6f" +
+            PrivateKeys.LUISSubscriptionKey +
             "&verbose=true&timezoneOffset=0&q=";
 
         public string CurrentQuery { get; set; }
--- NonEmergencyBot/NonEmergencyBot/PrivateKeys.cs
+++ NonEmergencyBot/NonEmergencyBot/PrivateKeys.cs
@@ -8,5 +8,7 @@ namespace NonEmergencyBot
     public static class PrivateKeys
     {
         public static string VisionApiKey = "secret";
+        public static string LUISApiKey = "secret";
+        public static string LUISSubscriptionKey = "secret";
     }
 }
